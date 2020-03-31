322c0e8a2f74706ce3a7c36dcce63270d4f2414f
solangejanus <solange@janusproj.com>
2020-03-30 22:07:51 -0300

    register-rsk-web3basic

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dapp-register-rsk
+# Register dapp - RSK
+
 Dapp RSK with some connection options and ways to interact with a basic smart contract to register information at Blockchain
+
+```shell
+git clone https://github.com/solangegueiros/dapp-register-rsk.git register-rsk
+```
---
+++ register-rsk-web3basic/index.js
@@ -0,0 +1,44 @@
+// Source code to interact with smart contract
+
+//connection with node
+var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:4444/'));
+
+// contractAddress and abi are setted after contract deploy
+var contractAddress = '0x73ec81da0c72dd112e06c09a6ec03b5544d26f05';
+var abi = JSON.parse( '[{"constant":true,"inputs":[],"name":"getInfo","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_info","type":"string"}],"name":"setInfo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]' );
+
+//contract instance
+contract = new web3.eth.Contract(abi, contractAddress);
+
+// Accounts
+var account;
+
+web3.eth.getAccounts(function(err, accounts) {
+  if (err != null) {
+    alert("Ocorreu um erro ao buscar suas contas.");
+    return;
+  }
+  if (accounts.length == 0) {
+    alert("Nenhuma conta encontrada! Verifique se o Ethereum client está configurado corretamente.");
+    return;
+  }
+  account = accounts[0];
+  console.log('Account: ' + account);
+  web3.eth.defaultAccount = account;
+});
+
+//Smart contract functions
+function registerSetInfo() {
+  info = $("#newInfo").val();
+  contract.methods.setInfo (info).send( {from: account}).then( function(tx) { 
+    console.log("Transaction: ", tx); 
+  });
+  $("#newInfo").val('');
+}
+
+function registerGetInfo() {
+  contract.methods.getInfo().call().then( function( info ) { 
+    console.log("info: ", info);
+    document.getElementById('lastInfo').innerHTML = info;
+  });    
+}
